001bb36ba541a05e89021ea47a20744f783ce169
hang.gao <yam51816@adobe.com>
2021-11-30 18:24:11 +0800

    Add new dialog field: button radio

---
+++ admin-base/ui.apps/src/main/content/jcr_root/apps/field/uxhub-button-radios/template.vue
@@ -0,0 +1,88 @@
+<template>
+	<div class="wrap">
+		<ul v-if="!schema.preview" :class="schema.inline ? 'radio-list inline' : 'radio-list'" :disabled="disabled" :id="getFieldID(schema)">
+		  <li v-for="item in items" :class="isItemChecked(item) ? 'checked' : ''">
+		    <input 
+		    	class="form-control"
+		    	:id="getItemName(item) + _uid"
+		    	type="radio" 
+		    	:class="schema.withGap ? 'with-gap' : ''"
+		    	:disabled="disabled" 
+		    	:name="id" 
+		    	@click="onSelection(item)" 
+		    	:value="getItemValue(item)" 
+		    	:checked="isItemChecked(item)"/>
+		    <!-- <label :for="getItemName(item)+_uid">{{ getItemName(item) }}</label> -->
+			<a :for="getItemName(item)+_uid" :title="getItemName(item)" class="waves-effect waves-light btn btn-small" @click="onSelection(item)" >
+				<i v-if="item.icon" class="material-icons left">{{item.icon}}</i>
+				<template v-if="schema.showItemLabel">
+				{{ getItemName(item) }}
+				</template>
+			</a>
+		  </li>
+		</ul>
+		<template v-else>
+			<p v-for="item in items" v-if="isItemChecked(item)">{{ getItemName(item) }}</p>
+		</template>
+	</div>
+</template>
+
+<script>	
+	export default {
+		mixins: [ VueFormGenerator.abstractField ], 
+
+		computed: {
+			items() {
+				let values = this.schema.values;
+				if (typeof(values) == "function") {
+					return values.apply(this, [this.model, this.schema]);
+				} else {
+					return values;
+				}
+			},
+			id(){
+				return this.schema.model;
+			}
+		},
+
+		methods: {
+			getItemValue(item) {
+				if (_.isObject(item)){
+					if (typeof this.schema["radiosOptions"] !== "undefined" && typeof this.schema["radiosOptions"]["value"] !== "undefined") {
+						return item[this.schema.radiosOptions.value];
+					} else {
+						if (typeof item["value"] !== "undefined") {
+							return item.value;
+						} else {
+							throw "`value` is not defined. If you want to use another key name, add a `value` property under `radiosOptions` in the schema. https://icebob.gitbooks.io/vueformgenerator/content/fields/radios.html#radios-field-with-object-values";
+						}
+					}
+				} else {
+					return item;
+				}
+			},
+			getItemName(item) {
+				if (_.isObject(item)){
+					if (typeof this.schema["radiosOptions"] !== "undefined" && typeof this.schema["radiosOptions"]["name"] !== "undefined") {
+						return item[this.schema.radiosOptions.name];
+					} else {
+						if (typeof item["name"] !== "undefined") {
+							return item.name;
+						} else {
+							throw "`name` is not defined. If you want to use another key name, add a `name` property under `radiosOptions` in the schema. https://icebob.gitbooks.io/vueformgenerator/content/fields/radios.html#radios-field-with-object-values";
+						}
+					}
+				} else {
+					return item;
+				}
+			},
+			onSelection(item) {
+				this.value = this.getItemValue(item);
+			},
+			isItemChecked(item) {
+				let currentValue = this.getItemValue(item);
+				return (currentValue === this.value);
+			},
+		}
+	};
+</script>
